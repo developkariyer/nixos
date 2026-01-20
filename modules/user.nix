9d79e28edc5f420b654999350a2ed8434a5001c5
Info Echr <info@echrautomation.org>
2026-01-20 17:17:11 +0100

    Splitted design

--- modules/user.nix
+++ modules/user.nix
@@ -11,8 +11,8 @@
   environment.shellAliases = {
     rebuild = "sudo nixos-rebuild switch --flake ~/Nixos-Setup#dell32";
     update = "nix flake update --flake ~/Nixos-Setup";
-    nanoconf = "nano ~/Nixos-Setup/configuration.nix";
-    nanoniri = "nano ~/Nixos-Setup/niri/config.kdl";
-    nanoflake = "nano ~/Nixos-Setup/flake.nix";
+    nn = "nano ~/Nixos-Setup/configuration.nix";
+    nr = "nano ~/Nixos-Setup/niri/config.kdl";
+    nf = "nano ~/Nixos-Setup/flake.nix";
   };
 }
